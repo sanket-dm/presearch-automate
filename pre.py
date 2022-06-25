ad25f5199d0c808c720b02c8941564c2dfd0e414
Sanket Deshmane <sanket.dm@outlook.com>
2022-06-25 08:20:13 +0530

    Update pre.py

--- pre.py
+++ pre.py
@@ -34,27 +34,27 @@ print("\n<---------- Round 1 ---------->\n")
 
 for i in range(1,36):
 	print ('iteration: ',i) 
-	url1 = f'https://engine.presearch.org/search?q={random.choice(resultantList1)}+{random.choice(resultantList1)}+{random.choice(resultantList1)}'
-	url2 = f'https://engine.presearch.org/search?q={random.choice(resultantList1)}+{random.choice(resultantList1)}+{random.choice(resultantList1)}'
-	url3 = f'https://engine.presearch.org/search?q={random.choice(resultantList1)}+{random.choice(resultantList1)}+{random.choice(resultantList1)}'
+	url1 = f'https://presearch.com/search?q={random.choice(resultantList1)}+{random.choice(resultantList1)}+{random.choice(resultantList1)}'
+	url2 = f'https://presearch.com/search?q={random.choice(resultantList1)}+{random.choice(resultantList1)}+{random.choice(resultantList1)}'
+	url3 = f'https://presearch.com/search?q={random.choice(resultantList1)}+{random.choice(resultantList1)}+{random.choice(resultantList1)}'
 
 	webbrowser.register('edge',
 		None,
 		webbrowser.BackgroundBrowser("C://Program Files (x86)//Microsoft//Edge//Application//msedge.exe"))
 	webbrowser.get('edge').open(url1)
-	time.sleep(random.uniform(1.5, 2))
+	time.sleep(random.uniform(2, 3))
 
 	webbrowser.register('chrome',
 		None,
 		webbrowser.BackgroundBrowser("C://Program Files//Google//Chrome//Application//chrome.exe"))
 	webbrowser.get('chrome').open(url2)
-	time.sleep(random.uniform(1.5, 2))
+	time.sleep(random.uniform(2, 3))
 	
 	webbrowser.register('brave',
 		None,
 		webbrowser.BackgroundBrowser("C://Program Files//BraveSoftware//Brave-Browser//Application//brave.exe"))
 	webbrowser.get('brave').open(url3)
-	time.sleep(random.uniform(1.5, 2))
+	time.sleep(random.uniform(2, 3))
 
 	time.sleep(random.randint(15,30))    
 	
@@ -73,27 +73,27 @@ print("\n<---------- Round 2 ---------->\n")
 
 for i in range(1,36):
 	print ('iteration: ',i) 
-	url4 = f'https://engine.presearch.org/search?q={random.choice(resultantList2)}+{random.choice(resultantList2)}+{random.choice(resultantList2)}'
-	url5 = f'https://engine.presearch.org/search?q={random.choice(resultantList2)}+{random.choice(resultantList2)}+{random.choice(resultantList2)}'
-	url6 = f'https://engine.presearch.org/search?q={random.choice(resultantList2)}+{random.choice(resultantList2)}+{random.choice(resultantList2)}'
+	url4 = f'https://presearch.com/search?q={random.choice(resultantList2)}+{random.choice(resultantList2)}+{random.choice(resultantList2)}'
+	url5 = f'https://presearch.com/search?q={random.choice(resultantList2)}+{random.choice(resultantList2)}+{random.choice(resultantList2)}'
+	url6 = f'https://presearch.com/search?q={random.choice(resultantList2)}+{random.choice(resultantList2)}+{random.choice(resultantList2)}'
 
 	webbrowser.register('edge-dev',
 		None,
 		webbrowser.BackgroundBrowser("C://Program Files (x86)//Microsoft//Edge Dev//Application//msedge.exe"))
 	webbrowser.get('edge-dev').open(url4)
-	time.sleep(random.uniform(1.5, 2))
+	time.sleep(random.uniform(2, 3))
 
 	webbrowser.register('edge-canary',
 		None,
 		webbrowser.BackgroundBrowser("C://Users//sanke//AppData//Local//Microsoft//Edge SxS//Application//msedge.exe"))
 	webbrowser.get('edge-canary').open(url5)
-	time.sleep(random.uniform(1.5, 2))
+	time.sleep(random.uniform(2, 3))
 
 	webbrowser.register('edge-beta',
 		None,
 		webbrowser.BackgroundBrowser("C://Program Files (x86)//Microsoft//Edge Beta//Application//msedge.exe"))
 	webbrowser.get('edge-beta').open(url6)
-	time.sleep(random.uniform(1.5, 2))
+	time.sleep(random.uniform(2, 3))
 
 	time.sleep(random.randint(15,30))    
 	
