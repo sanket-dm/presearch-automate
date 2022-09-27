2a212ac27e6811ac4c4ee0a7271d33460902042d
Sanket Deshmane <sanket.dm@outlook.com>
2022-09-27 20:51:03 +0530

    Update pre.py
    Simplified Code and updated the functions as per the latest update of the package random-words

--- pre.py
+++ pre.py
@@ -1,42 +1,29 @@
-print("Please wait 10 seconds... \nPopulating the lists...")
+print("Please wait a few seconds... \nPopulating the lists...\n")
 
 import webbrowser
 import time
 import random
 from random_word import RandomWords
 
-x = RandomWords()
+obj = RandomWords()
+randomWords = []
+for i in range(1,150):
+	randomWords.append(obj.get_random_word())
 
-randomWords1 = x.get_random_word()
-time.sleep(random.uniform(0.2, 0.5))
-randomWords2 = x.get_random_word()
+print("\nEnjoy your PRE tokens :)\n")
 
-time.sleep(1)
+resultantList = []
+for i in randomWords:
+    if i not in resultantList:
+        resultantList.append(i)
 
-randomWords3 = x.get_random_word()
-time.sleep(random.uniform(0.2, 0.5))
-randomWords4 = x.get_random_word()	
+print("\n<---------- Browser Set 1 ---------->\n")
 
-combinedWords1 = randomWords1 + randomWords2 
-combinedWords2 = randomWords3 + randomWords4
-
-resultantList1 = []
-for i in combinedWords1:
-    if i not in resultantList1:
-        resultantList1.append(i)
-
-resultantList2 = []
-for i in combinedWords2:
-    if i not in resultantList2:
-        resultantList2.append(i)
-
-print("\n<---------- Round 1 ---------->\n")
-
-for i in range(1,36):
-	print ('iteration: ',i) 
-	url1 = f'https://presearch.com/search?q={random.choice(resultantList1)}+{random.choice(resultantList1)}+{random.choice(resultantList1)}'
-	url2 = f'https://presearch.com/search?q={random.choice(resultantList1)}+{random.choice(resultantList1)}+{random.choice(resultantList1)}'
-	url3 = f'https://presearch.com/search?q={random.choice(resultantList1)}+{random.choice(resultantList1)}+{random.choice(resultantList1)}'
+for i in range(1,31):
+	print ('Current Iteration: ',i) 
+	url1 = f'https://presearch.com/search?q={random.choice(resultantList)}+{random.choice(resultantList)}+{random.choice(resultantList)}'
+	url2 = f'https://presearch.com/search?q={random.choice(resultantList)}+{random.choice(resultantList)}+{random.choice(resultantList)}'
+	url3 = f'https://presearch.com/search?q={random.choice(resultantList)}+{random.choice(resultantList)}+{random.choice(resultantList)}'
 
 	webbrowser.register('edge',
 		None,
@@ -56,9 +43,9 @@ for i in range(1,36):
 	webbrowser.get('brave').open(url3)
 	time.sleep(random.uniform(2, 3))
 
-	time.sleep(random.randint(15,30))    
+	time.sleep(random.randint(15,20))    
 	
-	if i%7 == 0:
+	if i%10 == 0:
 		import os
 		while 1 :
 			os.system("TASKKILL /F /IM msedge.exe")
@@ -69,13 +56,13 @@ for i in range(1,36):
 
 time.sleep(random.randint(10,15))
 
-print("\n<---------- Round 2 ---------->\n")
+print("\n<---------- Browser Set 2 ---------->\n")
 
-for i in range(1,36):
-	print ('iteration: ',i) 
-	url4 = f'https://presearch.com/search?q={random.choice(resultantList2)}+{random.choice(resultantList2)}+{random.choice(resultantList2)}'
-	url5 = f'https://presearch.com/search?q={random.choice(resultantList2)}+{random.choice(resultantList2)}+{random.choice(resultantList2)}'
-	url6 = f'https://presearch.com/search?q={random.choice(resultantList2)}+{random.choice(resultantList2)}+{random.choice(resultantList2)}'
+for i in range(1,31):
+	print ('Current Iteration: ',i) 
+	url4 = f'https://presearch.com/search?q={random.choice(resultantList)}+{random.choice(resultantList)}+{random.choice(resultantList)}'
+	url5 = f'https://presearch.com/search?q={random.choice(resultantList)}+{random.choice(resultantList)}+{random.choice(resultantList)}'
+	url6 = f'https://presearch.com/search?q={random.choice(resultantList)}+{random.choice(resultantList)}+{random.choice(resultantList)}'
 
 	webbrowser.register('edge-dev',
 		None,
@@ -95,9 +82,9 @@ for i in range(1,36):
 	webbrowser.get('edge-beta').open(url6)
 	time.sleep(random.uniform(2, 3))
 
-	time.sleep(random.randint(15,30))    
+	time.sleep(random.randint(15,20))    
 	
-	if i%7 == 0:
+	if i%10 == 0:
 		import os
 		while 1 :
 			os.system("TASKKILL /F /IM msedge.exe")
